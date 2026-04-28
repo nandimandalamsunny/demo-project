1735b98395e132ccaf1f38eaf5c6e8ada182ec2b
nandimandalamsunny-rootquotient <nandimandalam.sunny@rootquotient.com>
2026-04-29 00:35:48 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to React + Vite</h1>
+      <p>Your plain setup is ready for development.</p>
+    </div>
+  )
+}
+
+export default App
